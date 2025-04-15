cf7b80175516e871f23ffc6da45c6ec05d204b8a
dodo_kdy <dyn323@naver.com>
2025-04-15 20:13:50 +0900

    debug

--- kernel/trap.c
+++ kernel/trap.c
@@ -96,70 +96,54 @@ usertrap(void)
       struct proc *pr; 
       printf("DEBUG: Timer interrupt for proc[%d]: runtime=%lu, vruntime=%lu, timeslice=%d\n",
         p->pid, p->runtime, p->vruntime, p->timeslice);
-
-
     
     //eligible여기서 계산해둘것 = ∑((vi - v0) × wi) ≥ (vi - v0) × ∑wi
       //v0
       for(pr = proc; pr < &proc[NPROC]; pr++) {
-        if (pr != p) {
-          acquire(&pr->lock); 
-        }
-
+        if (pr == p) continue;
+        acquire(&pr->lock); 
         if(pr->state == RUNNABLE) {
           if (first){
             v0 = pr->vruntime;
             first=0;
           }
-
-          if (pr->vruntime < v0){
+          if (pr->vruntime < v0)
             v0 = pr->vruntime;
-          }
         }
-        if (pr != p) {
-          release(&pr->lock);
+        release(&pr->lock);
         }
-      }
-
-      // 좌변우변 
+      
+      
+        // 좌변우변 
       uint64 left=0,right=0,sum_w=0; 
       for(pr = proc; pr < &proc[NPROC]; pr++) {
-        if (pr != p) {
-          acquire(&pr->lock); 
-        }
+        if (pr == p) continue;
+        acquire(&pr->lock); 
         if(pr->state == RUNNABLE) {
           left += (pr->vruntime - v0) * pr->weight;
           sum_w += pr->weight;
         }
-        if (pr != p) {
-          release(&pr->lock);
-        }   
+        release(&pr->lock);
       }
-
       for(pr = proc; pr < &proc[NPROC]; pr++) {
+        if (pr==p) continue;
+        acquire(&pr->lock); 
         if(pr->state == RUNNABLE) {
-          if (pr != p) {
-            acquire(&pr->lock); 
-          }
           right =  (pr->vruntime - v0) * sum_w;
-          if (left >= right) {
-            pr -> eligible =1;
-          }
-          else{
-            pr -> eligible =0;
-          }
-
-        if (pr != p) {
-          release(&pr->lock);
-          }
+          if (left >= right) pr -> eligible =1;
+          else pr -> eligible =0;
         }
+        release(&pr->lock);
       }
-      release(&p->lock);
-      yield();     
     }
+    release(&p->lock);
+    yield();     
+    }
+    usertrapret();
   }
-  usertrapret();
-}
+  
+
+      
 
 // return to user space
 //
